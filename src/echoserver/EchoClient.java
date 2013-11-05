4253e196e6964ba97e6b51b88f6c4df9b596582f
Chase Ottomoeller <ottom005@eva01.morris.umn.edu>
2013-11-04 19:29:01 -0600

    broken client, working server enter the commit message for your changes. Lines starting

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -3,33 +3,85 @@ package echoserver;
 import java.net.*;
 import java.io.*;
 
-public class EchoClient {
+public class EchoClient implements Runnable{
 
-	public static void main(String[] args) {
-		try {
-			String serverIP;
-			if(args.length < 1){
-				serverIP = "localhost";
-			} else {
-				serverIP = args[0];
-			}
-			Socket socket = new Socket(serverIP, 6013);
-			
-			OutputStream toServer = socket.getOutputStream();
-			InputStream fromServer = socket.getInputStream();
-			InputStream fromKeyboard = System.in;
-			
-			int currentByte;
-			while ((currentByte = fromKeyboard.read()) != -1) {
-				toServer.write(currentByte);
-				System.out.write(fromServer.read());
-			}
-			System.out.flush();
-			socket.close();
-		} catch (IOException ioe) {
-			System.out.println("We caught an exception");
-			System.err.println(ioe);
-		}
-	}
+    OutputStream toServer;
+    InputStream fromServer;
+    InputStream fromKeyboard;
+    int mode;
+    Socket socket;
+
+    private EchoClient(OutputStream toServer, InputStream fromServer, InputStream fromKeyboard, int mode, Socket socket){
+        this.toServer = toServer;
+        this.fromServer = fromServer;
+        this.fromKeyboard = fromKeyboard;
+        this.mode = mode;
+        this.socket = socket;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        try {
+            String serverIP;
+            if(args.length < 1){
+                serverIP = "localhost";
+            } else {
+                serverIP = args[0];
+            }
+            Socket socket = new Socket(serverIP, 6013);
+
+            OutputStream toServer = socket.getOutputStream();
+            InputStream fromServer = socket.getInputStream();
+            InputStream fromKeyboard = System.in;
+
+
+            Thread toServerThread = new Thread(new EchoClient(toServer, fromServer, fromKeyboard, 0, socket));
+            Thread fromServerThread = new Thread(new EchoClient(toServer, fromServer, fromKeyboard, 1, socket));
+
+            toServerThread.start();
+            fromServerThread.start();
+
+            toServerThread.join();
+            fromServerThread.join();
+
+            System.out.flush();
+            socket.close();
+        } catch (IOException ioe) {
+            System.out.println("We caught an exception");
+            System.err.println(ioe);
+        }
+    }
+
+    @Override
+    public void run() {
+        if(mode == 0){
+            toServerThread();
+        } else {
+            fromServerThread();
+        }
+    }
+
+    private void toServerThread(){
+        try {
+            int currentByte;
+            while ((currentByte = fromKeyboard.read()) != -1) {
+                toServer.write(currentByte);
+            }
+        } catch (IOException ioe) {
+            System.out.println("We caught an exception");
+            System.err.println(ioe);
+        }
+    }
+
+    private void fromServerThread(){
+        try {
+            int currentByte;
+            while ((currentByte = fromKeyboard.read()) != -1) {
+                System.out.write(currentByte);
+            }
+        } catch (IOException ioe) {
+            System.out.println("We caught an exception");
+            System.err.println(ioe);
+        }
+    }
 
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -3,32 +3,53 @@ package echoserver;
 import java.net.*;
 import java.io.*;
 
-public class EchoServer {
+public class EchoServer implements Runnable {
 
-	public static void main(String[] args) {
-		try {
-			ServerSocket sock = new ServerSocket(6013);
+        Socket client;
+   
+        public EchoServer() {
+           
+        }
+       
+        private EchoServer(Socket client){
+            this.client = client;
+        }
+       
+    public static void main(String[] args){
+        try {
+            ServerSocket sock = new ServerSocket(6013);
+           
+            while (true) {
+                Socket client = sock.accept();
+                                System.out.println("Got a request!");
 
-			
-			
-			while (true) {
-				System.out.println("Got a request!");
-				Socket client = sock.accept();
+                Thread connection = new Thread(new EchoServer(client));
+                connection.start();
+            }
+           
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        }
+    }
 
-				OutputStream toClient = client.getOutputStream();
-				InputStream fromClient = client.getInputStream();
+    @Override
+    public void run() {
+        OutputStream toClient;
+        try {
+            toClient = client.getOutputStream();
+            InputStream fromClient = client.getInputStream();
+           
+            int currentByte;
+            while((currentByte=fromClient.read()) != -1){
+                            toClient.write(currentByte);
+            }
+            toClient.flush();
+            client.close();
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        }
 
-				int currentByte;
-				while((currentByte=fromClient.read()) != -1){
-						toClient.write(currentByte);
-				}
-				toClient.flush();
-				client.close();
-			}
-			
-		} catch (IOException ioe) {
-			System.err.println(ioe);
-		}
-	}
+
+    }
 
 }
